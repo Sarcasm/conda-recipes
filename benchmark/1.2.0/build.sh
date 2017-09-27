7fa8e779e0408d7f9b8f537455ee76cb80fdd501
Guillaume Papin <guillaume.papin@epitech.eu>
2017-09-27 22:19:30 +0200

    add google benchmark library

--- benchmark/1.2.0/build.sh
+++ benchmark/1.2.0/build.sh
@@ -7,9 +7,8 @@ pushd build
 cmake -G "${CMAKE_GENERATOR}" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      -DFMT_DOC=OFF \
       ..
-cmake --build . --config Release -- -j${CPU_COUNT}
+cmake --build . --config Release -- -j$CPU_COUNT
 ctest --build-config Release -j$CPU_COUNT
 cmake --build . --config Release --target install -- -j$CPU_COUNT
 popd
