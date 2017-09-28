ba5d5ea22965f41fceb7b64d5af86997b901cd3b
Guillaume Papin <guillaume.papin@epitech.eu>
2017-09-28 20:46:33 +0200

    produce a shared library for fmtlib
    So that it can be linked to shared libraries.
    Alternatively, one could set position independent code with:
    
       -DCMAKE_POSITION_INDEPENDENT_CODE=ON

--- fmt/4.0.0/build.sh
+++ fmt/4.0.0/build.sh
@@ -6,6 +6,7 @@ mkdir build
 pushd build
 cmake -G "${CMAKE_GENERATOR}" \
       -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DFMT_DOC=OFF \
       ..
