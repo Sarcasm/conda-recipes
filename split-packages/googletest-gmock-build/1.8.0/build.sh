eb7376859f8a08d8cf3569335d078beddd65632e
Guillaume Papin <guillaume.papin@epitech.eu>
2017-10-16 23:31:03 +0200

    add gmock by mean of a "split package"
    Building GMock builds GTest,
    so now both GMock and GTest depends on a "split package".
    The "split package" does not need to be published,
    it only needs to be built locally before GMock and GTest.

--- split-packages/googletest-gmock-build/1.8.0/build.sh
+++ split-packages/googletest-gmock-build/1.8.0/build.sh
@@ -5,14 +5,16 @@ set -o errexit
 mkdir build
 pushd build
 
-# tests aren't run as them seem to fail even though GTest is usable
+# tests aren't run because the GTest tests fails even though GTest seems usable
+# and the GMock tests uses too much CPU freezing the computer
 
 cmake -G "${CMAKE_GENERATOR}" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      -DBUILD_GMOCK=OFF \
+      -DBUILD_GMOCK=ON \
       -DBUILD_GTEST=ON \
       -Dgtest_build_tests=OFF \
+      -Dgmock_build_tests=OFF \
       ..
 cmake --build . --config Release -- -j$CPU_COUNT
 cmake --build . --config Release --target install -- -j$CPU_COUNT
