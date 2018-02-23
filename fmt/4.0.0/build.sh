8f86c6e2cdb510edb9fb0f2e19ccdf423a2835f3
Guillaume Papin <guillaume.papin@epitech.eu>
2018-02-23 23:39:46 +0100

    update fmt, add ninja

--- fmt/4.0.0/build.sh
+++ fmt/4.0.0/build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 mkdir build
 pushd build
-cmake -G "${CMAKE_GENERATOR}" \
+cmake -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=ON \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
