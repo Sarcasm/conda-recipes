c93892b03409b921aa733e668bd475f593e3c242
Guillaume Papin <guillaume.papin@epitech.eu>
2018-12-14 00:16:22 +0100

    clang wip

--- clang/build.sh
+++ clang/build.sh
@@ -1,82 +1,114 @@
 #!/bin/bash
 
 set -o errexit
-set -o nounset
-set -o pipefail
-set -o xtrace
 
-# -p is useful when building with --dirty
-mkdir -pv build
+mkdir -v build
 pushd build
 
-# CMake options that could be used for any other projects
-_cmake_config_defaults=(
-    # toolchain variables, which we expect conda to have exported
-    -DCMAKE_C_COMPILER="${CC}"
-    -DCMAKE_CXX_COMPILER="${CXX}"
-    -DCMAKE_AR="${AR}"
-    -DCMAKE_RANLIB="${RANLIB}"
-    -DCMAKE_CXX_COMPILER_AR="${GCC_AR}"
-    -DCMAKE_CXX_COMPILER_RANLIB="${GCC_RANLIB}"
-    -DCMAKE_C_COMPILER_AR="${GCC_AR}"
-    -DCMAKE_C_COMPILER_RANLIB="${GCC_RANLIB}"
-    -DCMAKE_NM="${NM}"
-    -DCMAKE_OBJCOPY="${OBJCOPY}"
-    -DCMAKE_OBJDUMP="${OBJDUMP}"
-    -DCMAKE_STRIP="${STRIP}"
-    -DCMAKE_LINKER="${LD}"
+# The ones you need for ARM are: ``libtinfo``, ``zlib1g``,
+# ``libxml2`` and ``liblzma``. In the Debian repository you'll
+# find downloads for all architectures.
+#
+# Turn off libxml? used only for testing code apparently, see CLANG_HAVE_LIBXML
+#
+# if (LLVM_ENABLE_LIBXML2 STREQUAL "FORCE_ON" AND NOT LLVM_LIBXML2_ENABLED)
+#   message(FATAL_ERROR "Failed to congifure libxml2")
+# endif()
+#
+# Disable enables colors?
+# option(LLVM_ENABLE_TERMINFO "Use terminfo database if available." ON)
+#
+# incorrect cache variables
+# grep / build/CMakeCache.txt | egrep -v '(^//|/home/papin_g/miniconda3/conda-bld)'
+# grep -nH --color /usr build/*.ninja
+#
+# Backtrace_INCLUDE_DIR:PATH=/usr/include
+# CMAKE_AR:FILEPATH=/usr/bin/ar
+# CMAKE_C_COMPILER_AR:FILEPATH=/usr/bin/gcc-ar
+# CMAKE_C_COMPILER_RANLIB:FILEPATH=/usr/bin/gcc-ranlib
+# CMAKE_CXX_COMPILER_AR:FILEPATH=/usr/bin/gcc-ar
+# CMAKE_CXX_COMPILER_RANLIB:FILEPATH=/usr/bin/gcc-ranlib
+# CMAKE_INSTALL_OLDINCLUDEDIR:PATH=/usr/include
+# CMAKE_LINKER:FILEPATH=/usr/bin/ld
+# CMAKE_NM:FILEPATH=/usr/bin/nm
+# CMAKE_OBJCOPY:FILEPATH=/usr/bin/objcopy
+# CMAKE_OBJDUMP:FILEPATH=/usr/bin/objdump
+# CMAKE_RANLIB:FILEPATH=/usr/bin/ranlib
+# CMAKE_STRIP:FILEPATH=/usr/bin/strip
+# DL_LIBRARY_PATH:FILEPATH=/usr/lib/libdl.so
+# GOLD_EXECUTABLE:FILEPATH=/usr/bin/ld.gold
+# GO_EXECUTABLE:FILEPATH=/usr/bin/go
+# ICONV_LIBRARY_PATH:FILEPATH=/usr/lib/libc.so
 
-    # It's not pretty,
-    # but CFLAGS and CXXFLAGS contains variables that needs to be expanded,
-    # and without this the CMake/Ninja combo does not expand them correctly.
-    # CFLAGS/CXXFLAGS have:
-    #    -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION}
-    #    -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix
-    # which expands to:
-    #    -fdebug-prefix-map==/usr/local/src/conda/-
-    #    -fdebug-prefix-map==/usr/local/src/conda-prefix
-    -DCMAKE_C_FLAGS="$(eval echo ${CFLAGS})"
-    -DCMAKE_CXX_FLAGS="$(eval echo ${CXXFLAGS})"
+# examples:
+# /home/papin_g/dev/github.com/AnacondaRecipes/aggregate/clangdev-feedstock/recipe/build.sh
+# llvmdev-feedstock/recipe/build.sh
+# /home/papin_g/dev/github.com/AnacondaRecipes/aggregate/mysql-feedstock/recipe/build.sh (see LD handling)
+# graphite2-feedstock/recipe/build.sh
 
-    # other standard variables
-    # PREFIX: 'host' requirements prefix, e.g. libncurses needed for build
-    # CONDA_PREFIX: 'build' requirements prefix, e.g. python needed for build
-    # not specified, because CMake is installed there, so it's in its default paths
-    # BUILD_PREFIX: same as PREFIX?
-    -DCMAKE_PREFIX_PATH="${PREFIX}"
-    -DCMAKE_INSTALL_PREFIX="${PREFIX}"
-    -DCMAKE_BUILD_TYPE=Release
+# document why reset $libdir
+# -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib
 
-    # disabled because conda removes it and replaces it with patchelf
-    -DCMAKE_SKIP_INSTALL_RPATH=ON
+# include(FindPythonInterp)
+# if( NOT PYTHONINTERP_FOUND )
+#   message(FATAL_ERROR
+# "Unable to find Python interpreter, required for builds and testing.
 
-    # these, I'm not sure, could vary between projects?
-    # or at least would benefit from
-    # being overridable by conda_build_config.yaml
-    -DCMAKE_INSTALL_LIBDIR=lib
-    -DBUILD_SHARED_LIBS=OFF
+# Please install Python or specify the PYTHON_EXECUTABLE CMake variable.")
+# endif()
+
+# if( ${PYTHON_VERSION_STRING} VERSION_LESS 2.7 )
+#   message(FATAL_ERROR "Python 2.7 or newer is required")
+# endif()
+
+# find_package(PythonInterp REQUIRED)
+
+# XXX: necessary because Clang uses `include(FindPythonInterp)',
+# instead of `find_package(PythonInterp)',
+# and CMAKE_PREFIX_PATH is not honored by the former.
+_cmake_config+=(
+    -DPYTHON_EXECUTABLE=${CONDA_PREFIX}/bin/python
 )
-
-declare -a _cmake_config
 
 _cmake_config+=(
-    -DGOLD_EXECUTABLE="${LD_GOLD}"
-
-    # needed for color output, a patch forces the use of libtinfo
-    -DLLVM_ENABLE_TERMINFO:BOOL=ON
-    -DLLVM_ENABLE_LIBEDIT:BOOL=ON
-
-    # Compiling/linking takes too much memory on my computer,
-    # limit parallelization so compilation works.
-    -DLLVM_PARALLEL_LINK_JOBS=1
-    -DLLVM_PARALLEL_COMPILE_JOBS=6
-
-    -DLLVM_LINK_LLVM_DYLIB=ON
+    -DGOLD_EXECUTABLE=${LD_GOLD}
 )
 
-cmake -G Ninja "${_cmake_config_defaults[@]}" "${_cmake_config[@]}" ..
+# CMAKE_LIBRARY_PATH is wrong but it should be harmless
+# as they do not actually use the value:
+#
+#     find_library(DL_LIBRARY_PATH dl)
+#     if (DL_LIBRARY_PATH)
+#       list(APPEND LIBS dl)
+#     endif()
+# It would probably be better to use CMAKE_DL_LIBS.
 
-# use ${VERBOSE_NINJA-} instead of just ${VERBOSE_NINJA},
-# which expands to VERBOSE_NINJA or the empty string if no set,
-# because script uses 'set -o nounset'
-ninja ${VERBOSE_NINJA-} install
+# document libxml2 used only for testing
+# LLVM_ENABLE_LIBXML2=OFF
+
+cmake -G Ninja \
+      -DCMAKE_PREFIX_PATH=${CONDA_PREFIX} \
+      -DCMAKE_C_COMPILER=${CC} \
+      -DCMAKE_CXX_COMPILER=${CXX} \
+      -DCMAKE_AR=${AR} \
+      -DCMAKE_RANLIB=${RANLIB} \
+      -DCMAKE_CXX_COMPILER_AR=${GCC_AR} \
+      -DCMAKE_CXX_COMPILER_RANLIB=${GCC_RANLIB} \
+      -DCMAKE_C_COMPILER_AR=${GCC_AR} \
+      -DCMAKE_C_COMPILER_RANLIB=${GCC_RANLIB} \
+      -DCMAKE_NM=${NM} \
+      -DCMAKE_OBJCOPY=${OBJCOPY} \
+      -DCMAKE_OBJDUMP=${OBJDUMP} \
+      -DCMAKE_STRIP=${STRIP} \
+      -DCMAKE_LINKER=${LD} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DLLVM_PARALLEL_LINK_JOBS=1 \
+      -DLLVM_PARALLEL_COMPILE_JOBS=6 \
+      -DLLVM_ENABLE_LIBXML2=OFF \
+      ${_cmake_config[@]} \
+      .. # /tmp/foopython            # ..
+
+# grep -i have_terminfo CMakeCache.txt
